912fc8aa91627d454155b238527deeef033cfe06
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-05-23 17:03:11 -0400

    Use a Vanilla API Contract
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,22 +1,6 @@
-import contract from "@bachman-dev/api-contract";
 import { fastify } from "fastify";
-import { initServer } from "@ts-rest/fastify";
 
 const app = fastify();
-
-const tsr = initServer();
-
-const router = tsr.router(contract, {
-  sayHello: async () =>
-    await Promise.resolve({
-      status: 200,
-      body: {
-        greeting: "Hello, World!",
-      },
-    }),
-});
-
-await app.register(tsr.plugin(router));
 
 try {
   await app.listen({ port: 3000 });
