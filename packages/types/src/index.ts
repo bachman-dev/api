eb87d8859235dbf351e0b7f40256709db3f61180
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-05-23 17:05:46 -0400

    Remove last ts-rest reference
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,18 +1,1 @@
-import { initContract } from "@ts-rest/core";
 import { z } from "zod";
-
-const tsr = initContract();
-
-const contract = tsr.router({
-  sayHello: {
-    method: "GET",
-    path: "/",
-    responses: {
-      200: z.object({
-        greeting: z.literal("Hello, World!"),
-      }),
-    },
-  },
-});
-
-export default contract;
