c8b516337c707629914af7f07f901760ea773d11
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-10-16 19:47:56 -0400

    Twitch OAuth database schema
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- packages/server/src/db/schema.ts
+++ packages/server/src/db/schema.ts
@@ -1,4 +1,38 @@
-// TODO: Define DB schemas
-const schema = {};
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { relations } from "drizzle-orm";
+
+export const twitchClients = sqliteTable("twitch_clients", {
+  clientId: text("client_id", { length: 30 }).primaryKey(),
+  name: text("name").notNull(),
+  clientSecret: text("client_secret", { length: 30 }).notNull(),
+});
+
+export const twitchClientStates = sqliteTable("twitch_client_states", {
+  state: text("state").primaryKey(),
+  clientId: text("client_id", { length: 30 })
+    .notNull()
+    .references(() => twitchClients.clientId),
+  status: text("status", { enum: ["pending", "completed", "cancelled"] }).notNull(),
+  accessToken: text("access_token"),
+});
+
+export const twitchClientRelations = relations(twitchClients, ({ many }) => {
+  return {
+    states: many(twitchClientStates),
+  };
+});
+
+export const twitchClientStatesRelations = relations(twitchClientStates, ({ one }) => {
+  return {
+    client: one(twitchClients),
+  };
+});
+
+const schema = {
+  twitchClients,
+  twitchClientStates,
+  twitchClientRelations,
+  twitchClientStatesRelations,
+};
 
 export default schema;
